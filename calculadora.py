dbe08d4b4db0410602fe1930bfeed4a95053c7eb
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-02-28 20:52:25 +0000

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+import os  # import no usado
+
+def sumar(a,b):
+    resultado=a+b
+    return resultado
+
+def dividir(a, b):
+    return a/b  # sin manejo de división por cero
+
+x = 10
+y = 0
+print(dividir(x,y))
